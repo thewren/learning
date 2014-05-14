146e22c006a6b36a52fec6139c7d19c701225075
Daniel Heinrich <danusestheinternet@gmail.com>
2014-05-13 21:53:57 -0700

    A working web server!

--- googleappenginepython/helloworld/helloworld.py
+++ googleappenginepython/helloworld/helloworld.py
@@ -9,7 +9,7 @@ class MainPage(webapp2.RequestHandler):
 #
 # Respond to a GET request with an HTTP header that describes the content in the response,
 # which is a text message that says Hello World
-def get(self):
+	def get(self):
 		self.response.headers['Content-Type'] = 'text/plain'
 		self.response.write('Hello, World!')
 
