8b5c32c3b96c3b0d29cef14278c941f29bc1c1d1
Daniel Heinrich <danusestheinternet@gmail.com>
2014-05-21 22:38:48 -0700

    fixes

--- googleappenginepython/guestbook/guestbook.py
+++ googleappenginepython/guestbook/guestbook.py
@@ -46,7 +46,7 @@ DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 
 # We set a parent key on the 'Greetings' to ensure that they are all in the same
 # entity group. Queries across the single entity group will be consistent.
-# However, the write rate should be limited to ~1/second.s
+# However, the write rate should be limited to ~1/second.
 
 def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
     """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
