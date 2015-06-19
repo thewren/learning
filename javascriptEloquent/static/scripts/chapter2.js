d46d07d38290a2cc53283bfc81f690040f59cd82
Daniel Heinrich <danusestheinternet@gmail.com>
2015-06-18 22:28:31 -0700

    Learning more

--- javascriptEloquent/static/scripts/chapter2.js
+++ javascriptEloquent/static/scripts/chapter2.js
@@ -10,6 +10,37 @@ http://eloquentjavascript.net/02_program_structure.html
 // Control Flow
 // http://eloquentjavascript.net/02_program_structure.html#h_rDxYNPd65Z
 
+
+/*
 var theNumber = Number(prompt("Pick a number", ""));
 alert("Your number is the square root of " + theNumber * theNumber + ".");
+*/
+
+// Conditional Execution
+// http://eloquentjavascript.net/02_program_structure.html#h_wpz5oi2dy7
+
+/*
+var theNumber = Number(prompt("Pick a number", ""));
+*/
+
+/*
+if (!isNaN(theNumber))
+	alert("Your number is the square root of " + theNumber * theNumber + ".");
+else
+	alert("Hey.  Why didn't you give me a number?");
+*/
+
+/*
+var num = Number(prompt("pick a number", "0"));
+
+if(num <10)
+	alert("Small");
+else if(num < 100)
+	alert("Medium");
+else
+	alert("Large");
+*/
+
+// While and Do Loops
+// http://eloquentjavascript.net/02_program_structure.html#h_FaGGgUI+MM
 
