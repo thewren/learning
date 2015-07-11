9c4bf1db13d61244201c8056d3ccedf00614d987
Daniel Heinrich <danusestheinternet@gmail.com>
2015-07-10 22:08:38 -0700

    More learning

--- javascriptEloquent/static/scripts/chapter3.js
+++ javascriptEloquent/static/scripts/chapter3.js
@@ -10,8 +10,10 @@ http://eloquentjavascript.net/03_functions.html
 // Defining a Function
 // http://eloquentjavascript.net/03_functions.html#h_tqLFw/oazr
 
-
 /*
-var theNumber = Number(prompt("Pick a number", ""));
-alert("Your number is the square root of " + theNumber * theNumber + ".");
+var square = function(x) {
+	return x * x;
+};
+
+console.log(square(12));
 */
