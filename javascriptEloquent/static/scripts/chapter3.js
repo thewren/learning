8a872494acb900f1748073585c8a1e8e5882e0b5
Daniel Heinrich <danusestheinternet@gmail.com>
2015-07-11 00:54:22 -0700

    More learning

--- javascriptEloquent/static/scripts/chapter3.js
+++ javascriptEloquent/static/scripts/chapter3.js
@@ -15,5 +15,43 @@ var square = function(x) {
 	return x * x;
 };
 
-console.log(square(12));
+var makeNoise = function() {
+	console.log("Pling!");
+}
+
+var power = function(base, exponent) {
+	var result = 1;
+	for (var count=0; count < exponent; count++) {
+		result *= base;
+	}
+	return result;
+}
+
 */
+
+
+
+// Parameters and Scopes
+
+/*
+var x = "outside";
+
+var f1 = function() {
+	var x = "inside f1";
+};
+
+f1();
+console.log(x);
+
+var f2 = function() {
+	x = "inside f2";
+};
+
+f2();
+console.log(x);
+*/
+
+
+
+// Nested Scopes
+
