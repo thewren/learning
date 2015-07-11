abaca80f4a06ade2054397959c6bd7d444185c20
Daniel Heinrich <danusestheinternet@gmail.com>
2015-07-10 22:05:31 -0700

    More learning

---
+++ javascriptEloquent/static/scripts/chapter3.js
@@ -0,0 +1,17 @@
+/*
+
+Chapter 3 - Functions
+http://eloquentjavascript.net/03_functions.html
+
+*/
+
+
+
+// Defining a Function
+// http://eloquentjavascript.net/03_functions.html#h_tqLFw/oazr
+
+
+/*
+var theNumber = Number(prompt("Pick a number", ""));
+alert("Your number is the square root of " + theNumber * theNumber + ".");
+*/
